db0a3cf32b7f2af77a7b77958251b34220d61570
tsahi atias <tsahiatias@gmail.com>
2022-03-08 15:10:22 +0200

    add webapi project

---
+++ WebApiDemo/Context/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Context
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
