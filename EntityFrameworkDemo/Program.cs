1c8622351c167f6216fb0cc0e13378f10515187f
Tsahi Atias <tsahiatias@gmail.com>
2022-05-19 10:50:00 +0300

    EF Demo

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using Dapper;
+using EntityFrameworkDemo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    internal class Program
+    {
+        // *** database script: https://raw.githubusercontent.com/microsoft/sql-server-samples/master/samples/databases/northwind-pubs/instnwnd%20(Azure%20SQL%20Database).sql
+
+        // *** Learn EF Core: https://www.entityframeworktutorial.net/efcore/entity-framework-core.aspx
+
+        // *** Learn Dapper: https://www.learndapper.com/selecting-single-rows
+
+        private static async Task Main(string[] args)
+        {
+            var context = new NorthwindContext();
+            var category = await
+                context.Categories
+                .Where(c => c.CategoryId == 1)
+                .Select(c => c.Description)
+                .FirstOrDefaultAsync();
+
+            Console.WriteLine(category);
+
+            Pause();
+
+            using (var conn = new SqlConnection(@"Password=CustomsDev123!;Persist Security Info=True;User ID=sa;Initial Catalog=Northwind;Data Source=localhost"))
+            {
+                var allCategories = await conn.QueryAsync<Category>("SELECT CategoryId, Description FROM Categories");
+                foreach (var item in allCategories)
+                {
+                    Console.WriteLine($"{item.CategoryId} - {item.Description}");
+                }
+            }
+
+            Pause();
+
+            var newCategory = new Category
+            {
+                CategoryName = "TestDemo",
+                Description = "Category Description TestDemo",
+                Picture = null
+            };
+
+            context.Categories.Add(newCategory);
+            var effectedRows = context.SaveChanges();
+            Console.WriteLine($"{effectedRows} row(s) effectged. CategoryID = {newCategory.CategoryId}");
+
+            Pause();
+
+            context.Dispose();
+            context = new NorthwindContext();
+
+            var deletedCategoty = new Category { CategoryId = newCategory.CategoryId };
+            context.Categories.Remove(deletedCategoty);
+            effectedRows = context.SaveChanges();
+            Console.WriteLine($"{effectedRows} row(s) effectged");
+
+            Pause();
+        }
+
+        private static void Pause()
+        {
+            Console.WriteLine(" === Press enter to continue ===");
+            Console.ReadLine();
+        }
+    }
+}
