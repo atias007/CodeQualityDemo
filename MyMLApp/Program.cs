6ae5d9badedddcf6b4c6fadff42580a6d7695f81
jer\tsahi_a <tsahi_a@malam.com>
2025-08-19 14:58:04 +0300

    45

---
+++ MyMLApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyMLApp;
+
+Console.WriteLine("Hello, World!");
+
+//Load sample data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = @"Crust feel like shit.",
+};
+
+//Load model and predict output
+var result = SentimentModel.Predict(sampleData);
+
+Console.WriteLine(result);
