2f5c9682ecd2d96ef725fbf91d9f0e0e0f9b228d
jer\tsahi_a <tsahi_a@malam.com>
2025-10-05 21:23:01 +0300

    f

---
+++ MyGenerator/MySourceGenerator .cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MyGenerator
+{
+    [Generator]
+    public class MySourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // Register for syntax notifications (optional)
+            // context.RegisterForSyntaxNotifications(() => new MySyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // Generate source code here
+            context.AddSource("GeneratedClass.g.cs",
+                @"
+                namespace MyGeneratedNamespace
+                {
+                    public static partial class GeneratedClass
+                    {
+                        public static void SayHello()
+                        {
+                            System.Console.WriteLine(""Hello from generated code!"");
+                        }
+                    }
+                }");
+        }
+    }
+}
